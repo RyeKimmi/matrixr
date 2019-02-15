d1a831c5c873b4ff5646289e839912f84c6442ea
Ryan Kim <skim19@stuy.edu>
2019-02-15 11:46:22 -0500

    upd

--- base_python/matrix.py
+++ base_python/matrix.py
@@ -12,28 +12,24 @@ import math
 #print the matrix such that it looks like
 #the template in the top comment
 def print_matrix( matrix ):
-#    ln = ''
-#    for i in matrix:
-#        for j in i:
-#            ln += str(j)
-#            ln += ' '
-#        ln = ln[:-1]
-#        ln += '\n'
-#    ln = ln[:-1]
-#    print(ln)
-
     ln = ''
-    for i in range( len( matrix ) ):
-        ln += str( matrix[i][j]
+    for i in range( len( matrix[0] ) ):
+        for j in range( len( matrix ) ):
+            ln += str( matrix[j][i] )
+            ln += ' '
+        ln = ln[:-1]
+        ln += '\n'
+    ln = ln[:-1]
+    print(ln)
 
 #turn the paramter matrix into an identity matrix
 #you may assume matrix is square
 def ident( matrix ):
     m = []
-    for c in range( len(matrix) ):
+    for c in range( len(matrix[0]) ):
         m.append( [] )
         i = 0
-        for r in range( len(matrix) ):
+        for r in range( len(matrix[0]) ):
             if i == c:
                 m[c].append( 1 )
             else:
@@ -44,18 +40,21 @@ def ident( matrix ):
 #multiply m1 by m2, modifying m2 to be the product
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
-    l1 = ''
-    l2 = ''
-    for i in m1:
+    m = new_matrix( len(m1[0]), len(m2) )
+    for i in m:
         for j in i:
-            l1 += str(j)
-    for k in m1:
-        for l in k:
-            l2 += str(j)
-            
+            j = (m1[i][0]*m2[0][j] + m1[i][1]*m2[1][j])
+    return m
 
-
-
+'''
+    m2[0][0] = m1[0][0]*m2[0][0] + m1[0][1]*m2[1][0]
+    m2[0][1] = m1[0][0]*m2[0][1] + m1[0][1]*m2[1][1]
+    m2[0][2] = m1[0][0]*m2[0][2] + m1[0][1]*m2[1][2]
+    
+    m2[1][0] = m1[1][0]*m2[0][0] + m1[1][1]*m2[1][0]
+    m2[1][1] = m1[1][0]*m2[0][1] + m1[1][1]*m2[1][1]
+    m2[1][2] = m1[1][0]*m2[0][2] + m1[1][1]*m2[1][2]
+'''
 
 def new_matrix(rows = 4, cols = 4):
     m = []
