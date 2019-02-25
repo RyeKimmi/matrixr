5f973ff2a0a3b1296f72f4de120bf66b264db495
Ryan Kim <skim19@stuy.edu>
2019-02-25 06:49:41 -0500

    moved files

---
+++ main.py
@@ -0,0 +1,26 @@
+from display import *
+from draw import *
+from matrix import *
+
+screen = new_screen()
+color = [ 0, 255, 0 ]
+
+#matrix = new_matrix(4,8)
+#print(matrix)
+#print_matrix(matrix)
+#print_matrix( ident(matrix) )
+
+mat1 = [[1, 2, 3], [4, 5, 6]]
+print( mat1 )
+print_matrix( mat1 )
+print_matrix( ident(mat1) )
+
+mat2 = [[1, 2], [3, 4], [5, 6]]
+print( mat2 )
+print_matrix( mat2 )
+print_matrix( ident(mat2) )
+
+print_matrix( matrix_mult(mat1, mat2) )
+
+#draw_lines( matrix, screen, color )
+#display(screen)
